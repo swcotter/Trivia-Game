38d1d670f5b2642093c7e3168403b3cc421c0adb
Samuel Cotter <swcotter@comcast.net>
2019-04-25 18:59:11 -0700

    Thursday Update

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// questions
+var questions = [
+    {
+        question: 'What object does Frank lose in the Ocean when the gang goes to the Jersey Shore?',
+        answers: ["His Gun", "The Rum Ham", "His Glasses", "Leather Jacket"],
+        answer: "The Rum Ham",
+        gifs: <iframe src="https://giphy.com/embed/V5aB8bPkmK796" width="480" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ham-V5aB8bPkmK796">via GIPHY</a></p>
+
+    },
+]
